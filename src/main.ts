f0aae990527e8ca86d9ffc8beebe75632e5cf6dd
Orangel <orangabriel18@gmail.com>
2022-08-15 17:50:21 -0400

    initial comit

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import './style.css'
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
+
+const card = document.getElementById('card');
+
+const ratebar = document.getElementById('ratebar');
+const btns = ratebar!.childNodes;
+
+
+
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const rate = e!.target!.innerText;
+    
+    const newCard = `
+
+      <div class="self-center justify-self-center p-3">
+        <img src="/illustration-thank-you.svg" alt="">
+      </div>
+
+      <span class="text-lg rounded-full p-2.5 bg-indigo-500 text-orange-500">You selected ${rate} out of 5</span>
+
+      <h1 class="text-2xl">Thank you!</h1>
+
+      <p class="text-lg text-indigo-300">We appreciate you taking the time to give a rating. If you ever need more support, 
+        don’t hesitate to get in touch!</p>
+
+    `;
+
+    Toastify({
+      text: 'Rating submitted succesfully',
+      position: 'center',
+      style: {
+        background: "linear-gradient(to right, rgb(99 102 241), rgb(99 102 241))",
+      }
+    }).showToast();
+
+    card!.innerHTML = newCard;
+  });
+});
